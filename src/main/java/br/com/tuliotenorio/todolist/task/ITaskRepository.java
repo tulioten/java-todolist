00ad965e5df96ccb2c289563d0285110c02089e7
tulioten <tuliotenorio0@gmail.com>
2024-09-06 15:26:15 -0300

    uploading java todolist

---
+++ src/main/java/br/com/tuliotenorio/todolist/task/ITaskRepository.java
@@ -0,0 +1,12 @@
+package br.com.tuliotenorio.todolist.task;
+
+import java.util.UUID;
+import java.util.List
+;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+}
